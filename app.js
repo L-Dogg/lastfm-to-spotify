c270b402e8803357728cdbf5b5aa2a19cde9bc49
L-Dogg <szymek94sul@gmail.com>
2017-02-05 00:26:28 +0100

    working proof of concept

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 (function(exports) {
 
-	var g_name = '';
+	var g_name = 'Loved tracks from last.fm';
 	var g_tracks = '';
 
     var id = '';
+
+    var prefix = 'spotify:track:';
 
 	var findOnSpotify = function(query) {
         $.ajax({
@@ -15,7 +17,10 @@
 				limit: '1'
             },
             success: function (response) {
+        	    // todo error handling if undefined
 				id = response.tracks.items[0].id;
+
+                g_tracks = g_tracks + (prefix + id) + ",";
 				console.log(id);
             },
 			error: function (response) {
@@ -24,10 +29,10 @@
         });
 	}
 
-	var getTracksFromLastfm = function (userName) {
+	var getTracksFromLastfm = function (userName, limit) {
 		$.ajax({
 			url: "http://ws.audioscrobbler.com/2.0/?method=user.getlovedtracks&user=" + userName +
-				"&api_key=5275ac1e04b48394d83abd58eccc0cce&format=json&limit=10",
+				"&api_key=5275ac1e04b48394d83abd58eccc0cce&format=json&limit=" + limit,
             error: function (response) {
                 console.log(response);
             },
@@ -40,6 +45,7 @@
     var tracks = new Array();
 
 	var clearList = function () {
+        g_tracks = '';
         var ul = document.getElementById("trackList");
         while (ul.firstChild) {
             ul.removeChild(ul.firstChild);
@@ -63,21 +69,20 @@
 
             var li = document.createElement("li");
             li.className = "list-group-item";
-            li.appendChild(document.createTextNode(current.artist + " - " + current.song + " / " + current.spotifyId));
+            li.appendChild(document.createTextNode(current.artist + " - " + current.song));
             ul.appendChild(li);
-
         })
     }
 
 	client_id = '3e1b5000798543d9acea810a30616b17';
-	redirect_uri = 'http://localhost:8000/callback.html';
+	redirect_uri = 'http://localhost/callback.html';
 
 	var login = function(callback) {
 		var url = 'https://accounts.spotify.com/authorize?client_id=' + client_id +
 			'&response_type=token' +
 			'&scope=playlist-read-private%20playlist-modify%20playlist-modify-private' +
 			'&redirect_uri=' + encodeURIComponent(redirect_uri);
-		localStorage.setItem('createplaylist-tracks', JSON.stringify(g_tracks));
+		localStorage.setItem('createplaylist-tracks', g_tracks.slice(0, -1));
 		localStorage.setItem('createplaylist-name', g_name);
 		var w = window.open(url, 'window', 'WIDTH=400,HEIGHT=500');
 	}
@@ -88,7 +93,7 @@
 		})
 		$('#loadLoved').click(function (event) {
             event.preventDefault();
-            getTracksFromLastfm("orggg");
+            getTracksFromLastfm("orggg", 200);
         });
 }
 
---
+++ todo.md
@@ -0,0 +1,6 @@
+todo:
+    * error handling
+    * passing many tracks
+    * styling
+    * list checkboxes to edit playlist
+    * username box
